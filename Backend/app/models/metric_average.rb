9a7267efd3961be0c2c23dd71c93f2ffc60cd1ef
Ivan Luque <ivanonchi@gmail.com>
2024-05-01 11:07:19 +0200

    Rewrite as generic case for any timespan, and adds tests.

--- Backend/app/models/metric_average.rb
+++ Backend/app/models/metric_average.rb
@@ -1,43 +1,52 @@
+# Stores the average value of a metric in a timespan
 class MetricAverage < ApplicationRecord
   enum :timespan, { minute: 'minute', hour: 'hour', day: 'day' }
 
   class << self
     def calculate_averages
-      averages_per_minute
-      # averages_per_hour
-      # averages_per_day
+      averages_per_timespan('minute')
+      averages_per_timespan('hour')
+      averages_per_timespan('day')
     end
 
-    def averages_per_minute
-      last_minute = 1.minute.ago.beginning_of_minute
+    def averages_per_timespan(timespan)
+      last_timespan = 1.public_send(timespan).ago.public_send("beginning_of_#{timespan}")
+      last_average = MetricAverage.public_send(timespan).last
+      return unless last_average.nil? || last_average.timestamp < last_timespan
 
       # Process old metrics in case there was some issue
-      last_average = MetricAverage.minute.last
+      process_timespan_averages_until(timespan, last_timespan)
+
+      # Calculate previous minute average
+      average_for(timespan, last_timespan)
+    end
+
+    private
+
+    def process_timespan_averages_until(timespan, last_timespan)
+      last_average = MetricAverage.public_send(timespan).last
       if last_average.nil? && Metric.first.present?
-        process_old_metrics(Metric.first.timestamp, last_minute)
-      elsif last_average.timestamp < last_minute
-        process_old_metrics(last_average.timestamp, last_minute)
-      end
-
-      # Process previous minute metrics
-      average_for_minute(last_minute)
-    end
-
-    def process_old_metrics(from_minute, to_minute)
-      current_minute = from_minute
-      while current_minute < to_minute
-        average_for_minute(current_minute)
-        current_minute += 1.minute
+        process_old_averages(timespan, Metric.first.timestamp, last_timespan)
+      elsif last_average.timestamp < last_timespan
+        process_old_averages(timespan, last_average.timestamp + 1.public_send(timespan), last_timespan)
       end
     end
 
-    def average_for_minute(minute)
-      minute_beginning = minute.beginning_of_minute
-      minute_end = minute_beginning.end_of_minute
-      metrics = Metric.where(timestamp: minute_beginning..minute_end)
+    def process_old_averages(timespan, from, to)
+      current = from
+      while current < to
+        average_for(timespan, current)
+        current += 1.public_send(timespan)
+      end
+    end
+
+    def average_for(timespan, datetime)
+      timespan_beginning = datetime.public_send("beginning_of_#{timespan}")
+      timespan_end = timespan_beginning.public_send("end_of_#{timespan}")
+      metrics = Metric.where(timestamp: timespan_beginning..timespan_end)
       averages = metrics.group(:name).average(:value)
       averages.each do |metric_name, average|
-        MetricAverage.create!(name: metric_name, average: average, timestamp: minute_beginning, timespan: 'minute')
+        MetricAverage.create!(name: metric_name, average: average, timestamp: timespan_beginning, timespan: timespan)
       end
     end
   end
--- Backend/spec/models/metric_average_spec.rb
+++ Backend/spec/models/metric_average_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe MetricAverage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'averages_per_timespan' do
+    before do
+      create(:metric, name: 'pv', value: '10', timestamp: '2024-04-30 15:59:00')
+      create(:metric, name: 'pv', value: '1', timestamp: '2024-04-30 16:00:00')
+      create(:metric, name: 'pv', value: '2', timestamp: '2024-04-30 16:00:01')
+      create(:metric, name: 'pv', value: '3', timestamp: '2024-04-30 16:00:02')
+      create(:metric, name: 'pv', value: '4', timestamp: '2024-04-30 16:01:00')
+    end
+
+    it 'calculates minute averages' do
+      travel_to '2024-04-30 16:01:00'
+      expect { MetricAverage.averages_per_timespan('minute') }.to change(MetricAverage, :count).by(2)
+      expect(MetricAverage.first.average).to eq(10)
+      expect(MetricAverage.second.average).to eq(2)
+    end
+
+    it 'calculates hour averages' do
+      travel_to '2024-04-30 17:00:00'
+      expect { MetricAverage.averages_per_timespan('hour') }.to change(MetricAverage, :count).by(2)
+      expect(MetricAverage.first.average).to eq(10)
+      expect(MetricAverage.second.average).to eq(2.5)
+    end
+
+    it 'calculates day averages' do
+      travel_to '2024-05-01 00:00:00'
+      expect { MetricAverage.averages_per_timespan('day') }.to change(MetricAverage, :count).by(1)
+      expect(MetricAverage.first.average).to eq(4)
+    end
+  end
 end
